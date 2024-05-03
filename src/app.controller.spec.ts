39c634c242cde3dd880dd3b761f28a17c9cae4ee
nazishasghar <72311025+nazishasghar@users.noreply.github.com>
2024-05-04 02:02:26 +0900

    [modify] husky added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,21 +1,15 @@
-// import { Test, TestingModule } from '@nestjs/testing'
-// import { AppController } from './app.controller'
-
-// describe('AppController', () => {
-//     let appController: AppController
-
-//     beforeEach(async () => {
-//         const app: TestingModule = await Test.createTestingModule({
-//             controllers: [AppController],
-//             providers: [AppService],
-//         }).compile()
-
-//         appController = app.get<AppController>(AppController)
-//     })
-
-//     describe('root', () => {
-//         it('should return "Hello World!"', () => {
-//             // expect(appController.getHello()).toBe('Hello World!')
-//         })
-//     })
-// })
+describe.skip('AppController', () => {
+    // let appController: AppController
+    // beforeEach(async () => {
+    //     const app: TestingModule = await Test.createTestingModule({
+    //         controllers: [AppController],
+    //         providers: [AppService],
+    //     }).compile()
+    //     appController = app.get<AppController>(AppController)
+    // })
+    describe('root', () => {
+        it('should return "Hello World!"', () => {
+            // expect(appController.getHello()).toBe('Hello World!')
+        })
+    })
+})
